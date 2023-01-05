ce478eb23f3664806b46f9a82ed5178303302686
Piotr <pio.andr@gmail.com>
2023-01-06 00:35:28 +0100

    Add float type size check in python

--- python/int_type_size.py
+++ python/int_type_size.py
@@ -1,6 +1,8 @@
 import sys
 
-# The idea is to check how python integer size changes with it's value
+# The idea is to check how python integer and float size changes with it's value
+# For 64 bit architecture integer object is 24 bytes without stored value and it raises
+# with integer value stored. Float object is always 24 bytes.
 
 def print_value_and_size(value_string: str, size:int, address: int) -> None:
     print(f"Value: {value_string} \t Size: {size} bytes \t Address: {hex(address)}")
@@ -8,18 +10,17 @@ def print_value_and_size(value_string: str, size:int, address: int) -> None:
 def size_has_changed(previous_size: int, current_size: int) -> bool:
     return previous_size != current_size
 
-integer = 0
-print_value_and_size(f"{str(integer)}", sys.getsizeof(integer), id(integer))
+def multiply_value_by_2_and_print(input_value, num_of_repetition: int) -> None:
+    previous_size = 0
+    value = input_value
+    for i in range(0, num_of_repetition):
+        value = value * 2
+        current_size = sys.getsizeof(value) 
+        if size_has_changed(previous_size, current_size):
+            print_value_and_size(f"{input_value}*2^{i}", current_size, id(value))
+            previous_size = current_size
 
-integer = -1
-print_value_and_size(f"{str(integer)}", sys.getsizeof(integer), id(integer))
 
-integer = 1
-previous_size = 0
-
-for i in range(0, 1000):
-    integer *= 2
-    current_size = sys.getsizeof(integer)
-    if size_has_changed(previous_size, current_size):
-        print_value_and_size(f"2^{i}", current_size, id(integer))
-        previous_size = current_size
+if __name__ == "__main__":
+    multiply_value_by_2_and_print(1, 1000)
+    multiply_value_by_2_and_print(1.0, 1000)
