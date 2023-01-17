f86c3501772fa93de29c8f5279db0f0e35319017
Piotr <pio.andr@gmail.com>
2023-01-17 21:42:35 +0100

    Add manual paramaters parsing example

---
+++ bash/parameters_parsing_manual.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+function print_help_check_advice() {
+    echo "Try \"$0 --help\" for more information."
+}
+
+function print_help() {
+    echo "Usage: 
+    $0 [options] [-f FILE]  
+Options:
+    -h, --help                    Print this help message
+    -a, --a_parameters            Use \"a\" option
+    -b, --b_parameters            Use \"b\" option
+    -c, --c_parameters            Use \"c\" option
+    -f, --file {FILENAME}         Filename of a file to process"
+}
+
+if [ $# -eq 0 ]; then
+    print_help_check_advice
+fi
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -a|--a_parameter )
+            echo "Using a parameter."
+            shift
+            ;;
+        -b|--b_parameter )
+            echo "Using b parameter."
+            shift
+            ;;
+        -c|--c_parameter )
+            echo "Using c parameter."
+            shift
+            ;;
+        -h|--help )
+            print_help
+            exit 0
+            ;;
+        -f|--file )
+            FILENAME="$2"
+            if ! [ -f "$FILENAME" ]; then
+                echo "ERROR: File \"$FILENAME\" does not exist."
+                exit 2
+            fi
+            echo "File \"$FILENAME\" will be used for processing."
+            shift
+            shift
+            ;;
+        * )
+            echo "ERROR: Unknown parameter \"$1\" used."
+            print_help_check_advice
+            exit 1
+            ;;
+    esac
+done
