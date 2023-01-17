11addb43cfd14ef1f82ba67cfe8749feeb96077e
Piotr <pio.andr@gmail.com>
2023-01-17 22:13:23 +0100

    Add parameters parsing using getopts

---
+++ bash/parameters_parsing_getopts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# It provides an easy way for parsing positional parameters.
+# However it doesn't work for long positional parameters form.
+
+while getopts abcf: parameter; do
+    case $parameter in
+        a) echo "Using a parameter."
+            ;;
+        b) echo "Using b parameter."
+            ;;
+        c) echo "Using c parameter."
+            ;;
+        f)  FILENAME="$OPTARG"
+            if ! [ -f "$FILENAME" ]; then
+                echo "ERROR: File \"$FILENAME\" does not exist."
+                exit 2
+            fi
+            echo "File \"$FILENAME\" will be used for processing."
+            ;;
+    esac
+done
